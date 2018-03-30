7f49e5a08d77cabd3f04e1870bc47ece634b27ff
tamassneider <tamassneider90@gmail.com>
2018-03-30 10:58:24 +0200

    -added structuring to event list with loop in loop

--- src/app/event/event-list/event-list.component.ts
+++ src/app/event/event-list/event-list.component.ts
@@ -8,14 +8,22 @@ import {EventModel} from '../../shared/event-model';
   styleUrls: ['./event-list.component.css']
 })
 export class EventListComponent implements OnInit {
-  public events: EventModel[];
+  public eventsGrouppedBy3: EventModel[];
 
   constructor(private _eventService: EventService) {
-    this.events = this._eventService.getAllEvents();
-    console.log(this.events);
   }
 
   ngOnInit() {
+    // [0,1,2,3,4,5,6,7,8] -- reduce --> [[0,1,2],[3,4,5],[6,7,8]]
+    this.eventsGrouppedBy3 = this._eventService.getAllEvents()
+      .reduce((acc, curr: EventModel, ind: number) => {
+        if (ind % 3 === 0) {
+          acc.push([]);
+        }
+        acc[acc.length - 1].push(curr);
+        return acc;
+      }, []);
+    console.log(this.eventsGrouppedBy3);
   }
 
 }
