2d94ef36d3bbfec92d61c20ad1c604c60354665e
tamassneider <tamassneider90@gmail.com>
2018-04-09 12:56:22 +0200

    - added an output to licit-card component, which can in turn interact with the licit component through a new method added there

--- src/app/ticket/licit-card/licit-card.component.ts
+++ src/app/ticket/licit-card/licit-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TicketModel} from '../../shared/ticket-model';
 
 @Component({
@@ -8,5 +8,9 @@ import {TicketModel} from '../../shared/ticket-model';
 })
 export class LicitCardComponent {
   @Input() ticket: TicketModel;
+  @Output() bidWithBidStep = new EventEmitter<void>();
 
+  onBidWithBidStep() {
+    this.bidWithBidStep.emit();
+  }
 }
