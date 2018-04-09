90481e4b8fb986fc4ee79e509de2bc39c481a35e
tamassneider <tamassneider90@gmail.com>
2018-04-09 11:42:54 +0200

    - added getOne in licit component ts and html

--- src/app/ticket/licit/licit.component.ts
+++ src/app/ticket/licit/licit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TicketService} from '../../shared/ticket.service';
+import {TicketModel} from '../../shared/ticket-model';
 
 @Component({
   selector: 'app-licit',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./licit.component.css']
 })
 export class LicitComponent implements OnInit {
+  ticket: TicketModel;
 
-  constructor() { }
+  constructor( private _ticketService: TicketService) { }
 
   ngOnInit() {
+    const id = '-L9QY6vA9XNd8Zb3TCuI';
+    this._ticketService.getOne(id).subscribe(
+      ticket => this.ticket = ticket
+    );
   }
 
 }
