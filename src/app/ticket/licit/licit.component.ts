369526d57278905272f96c5a4c1fecec5a70586b
tamassneider <tamassneider90@gmail.com>
2018-04-10 09:18:01 +0200

    - licit component handle error when no id or invalid id is received

--- src/app/ticket/licit/licit.component.ts
+++ src/app/ticket/licit/licit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {TicketService} from '../../shared/ticket.service';
 import {TicketModel} from '../../shared/ticket-model';
 import {UserService} from '../../shared/user.service';
-import {ActivatedRoute, ParamMap} from '@angular/router';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 
 @Component({
   selector: 'app-licit',
@@ -15,15 +15,29 @@ export class LicitComponent implements OnInit {
 
   constructor( private _ticketService: TicketService,
                userService: UserService,
-               private _route: ActivatedRoute) {
+               private _route: ActivatedRoute,
+               private _router: Router) {
     this.isLoggedIn = userService.isLoggedin;
   }
 
   ngOnInit() {
+    const handle404 = () => {
+      this._router.navigate(['404']);
+    };
+
     this._route.paramMap.subscribe(
       (params: ParamMap) => {
         this._ticketService.getOne(params.get('id')).subscribe(
-          ticket => this.ticket = ticket
+          ticket => {
+            if (ticket === null) {
+              handle404();
+            } else {
+              this.ticket = ticket;
+            }
+          },
+          err => {
+            return handle404();
+          }
         );
       }
     );
