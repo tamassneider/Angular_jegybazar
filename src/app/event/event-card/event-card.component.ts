b77b5a0235c2d08b00ccb5542b52e6eab4e431d0
tamassneider <tamassneider90@gmail.com>
2018-04-13 10:32:12 +0200

    - added change detector to event card

--- src/app/event/event-card/event-card.component.ts
+++ src/app/event/event-card/event-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {EventModel} from '../../shared/event-model';
 
 @Component({
@@ -6,12 +6,37 @@ import {EventModel} from '../../shared/event-model';
   templateUrl: './event-card.component.html',
   styleUrls: ['./event-card.component.css']
 })
-export class EventCardComponent implements OnInit {
+export class EventCardComponent implements AfterViewInit, OnChanges {
   @Input () esemeny: EventModel
   @Input () nextLabel = 'Tovább';
-  constructor() { }
+  constructor(private _cdr: ChangeDetectorRef) { }
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    this._cdr.detach();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['nextLabel'] != null
+      && changes ['nextLabel'].isFirstChange()) {
+      this._cdr.detectChanges();
+    } else if (changes['esemeny'] != null) {
+      const prev: EventModel = changes['esemeny'].previousValue;
+      const current: EventModel = changes['esemeny'].currentValue;
+
+      if (prev == null || current == null) {
+        this._cdr.detectChanges();
+      } else if (prev.pictureURL !== current.pictureURL) {
+        this._cdr.detectChanges();
+      } else if (prev.name !== current.name) {
+        this._cdr.detectChanges();
+      } else if (prev.date !== current.date) {
+        this._cdr.detectChanges();
+      } else if (prev.description !== current.description) {
+        this._cdr.detectChanges();
+      } else if (prev.id !== current.id) {
+        this._cdr.detectChanges();
+      }
+    }
   }
 
 }
