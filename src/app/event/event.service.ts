2963111022be5e9594d78b37cbbf910210d8844c
tamassneider <tamassneider90@gmail.com>
2018-04-18 12:24:16 +0200

    - refactor events service - fixes in detail component

--- src/app/event/event.service.ts
+++ src/app/event/event.service.ts
@@ -5,120 +5,48 @@ import {environment} from '../../environments/environment.prod';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
+import {AngularFireDatabase} from 'angularfire2/database';
+import 'rxjs/add/observable/fromPromise';
+import * as firebase from 'firebase';
+import Reference = firebase.database.Reference;
 
 @Injectable()
 export class EventService {
   // private _events: EventModel[];
 
-  constructor(private _http: HttpClient) {
-   // this._events = this.getMockData ();
-  }
+  constructor(private _afDb: AngularFireDatabase) {
 
-  // getMockData() {
-  //   return [
-  //     new EventModel(
-  //   {
-  //     'id': 1,
-  //     'name': 'Sziget',
-  //     'date': '2018-08-03',
-  //     'pictureURL': 'assets/sziget.png',
-  //     'description': 'Lorem ipsum dolor sit amet.'
-  //   }),
-  //     new EventModel(
-  //   {
-  //     'id': 2,
-  //     'name': 'Diótörő Balett',
-  //     'date': '2018-11-23',
-  //     'pictureURL': 'assets/diotoro.jpg',
-  //     'description': 'Lorem ipsum dolor sit amet.'
-  //   }),
-  //   {
-  //     'id': 3,
-  //     'name': 'Sziget',
-  //     'date': '2018-08-03',
-  //     'pictureURL': 'assets/sziget.png',
-  //     'description': 'Lorem ipsum dolor sit amet.'
-  //   },
-  //     new EventModel(
-  //   {
-  //     'id': 4,
-  //     'name': 'Diótörő Balett',
-  //     'date': '2018-11-23',
-  //     'pictureURL': 'assets/diotoro.jpg',
-  //     'description': 'Lorem ipsum dolor sit amet.'
-  //   }),
-  //     new EventModel(
-  //   {
-  //     'id': 5,
-  //     'name': 'Sziget',
-  //     'date': '2018-08-03',
-  //     'pictureURL': 'assets/sziget.png',
-  //     'description': 'Lorem ipsum dolor sit amet.'
-  //   }),
-  //     new EventModel(
-  //   {
-  //     'id': 6,
-  //     'name': 'Diótörő Balett',
-  //     'date': '2018-11-23',
-  //     'pictureURL': 'assets/diotoro.jpg',
-  //     'description': 'Lorem ipsum dolor sit amet.'
-  //   }),
-  //     new EventModel(
-  //   {
-  //     'id': 7,
-  //     'name': 'Sziget',
-  //     'date': '2018-08-03',
-  //     'pictureURL': 'assets/sziget.png',
-  //     'description': 'Lorem ipsum dolor sit amet.'
-  //   }),
-  //     new EventModel(
-  //   {
-  //     'id': 8,
-  //     'name': 'Diótörő Balett',
-  //     'date': '2018-11-23',
-  //     'pictureURL': 'assets/diotoro.jpg',
-  //     'description': 'Lorem ipsum dolor sit amet.'
-  //   }),
-  //     new EventModel(
-  //   {
-  //     'id': 9,
-  //     'name': 'Diótörő Balett',
-  //     'date': '2018-11-23',
-  //     'pictureURL': 'assets/diotoro.jpg',
-  //     'description': 'Lorem ipsum dolor sit amet.'
-  //   })];
-  // }
+  }
 
   getAllEvents(): Observable<EventModel[]> {
-    return this._http.get<EventModel[]>(`${environment.firebase.baseUrl}/events.json`)
-    .map(data => Object.values(data));
+    return this._afDb.list<EventModel>(`events`).valueChanges()
+    //   .map(
+    //   events =>
+    //     events.map(event = new EventModel(events))
+    // );
   }
   getEventById(id: string) {
-    return this._http.get<EventModel>(`${environment.firebase.baseUrl}/events/${id}.json`);
+    return this._afDb.object<EventModel>(`events/${id}`).valueChanges()
   }
 
   save(param: EventModel) {
-    console.log(param);
-  if (param.id)  { // update event
-    return this._http.put(`${environment.firebase.baseUrl}/events/${param.id}.json`,param);
-  } else { // create event
-    return this._http.post(`${environment.firebase.baseUrl}/events.json`, param)
-      .map((fbPostReturn: {name: string }) => fbPostReturn.name)
-      .switchMap(fbId => this._http.patch(
-        `${environment.firebase.baseUrl}/events/${fbId}.json`, {id: fbId}));
-  }
+    if (param.id) {
+      return Observable.fromPromise(this._afDb
+        .object(`events/${param.id}`).update(param));
+    } else {
+      return Observable.fromPromise(this._afDb
+        .object(`events/${param.id}`).set(param));
+    }
   }
 
   delete(param: EventModel) {
-    return this._http.delete(`${environment.firebase.baseUrl}/events/${param.id}.json`)
+    return Observable.fromPromise (this._afDb
+      .object(`events/${param.id}`).remove());
   }
 
-  addTicket(eventId: string, ticketId: string): Observable<string> {
-    return this._http.patch(
-      `${environment.firebase.baseUrl}/events/${eventId}/tickets.json`,
-      {[ticketId]: true}
-    )
-      .map(rel => Object.keys(rel)[0]);
+  addTicket(eventId: string, ticketId: string): Observable<Reference> {
+    return Observable.fromPromise(this._afDb.list(`events/${eventId}/tickets`)
+      .push(ticketId));
   }
 
 //   private _getMaxId() {
