7a559e7100585c56bcfc064a9bece5ef04ebdd7d
tamassneider <tamassneider90@gmail.com>
2018-04-19 09:22:57 +0200

    - addded isLoggedIn to app component - chat component: enabled Onpush - chat window: adjusted css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {UserService} from './shared/user.service';
+import {ReplaySubject} from 'rxjs/ReplaySubject';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isLoggedIn$: ReplaySubject<boolean>;
 
-
+  constructor(userService: UserService) {
+    this.isLoggedIn$ = userService.isLoggedIn$;
+  }
 
 }
