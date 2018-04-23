af6740d432208a38d994b67da5c303381bf82288
tamassneider <tamassneider90@gmail.com>
2018-04-23 12:19:08 +0200

    -translate with service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {UserService} from './shared/user.service';
 import {ReplaySubject} from 'rxjs/ReplaySubject';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,18 @@ export class AppComponent {
   isLoggedIn$: ReplaySubject<boolean>;
   translateVariable = {variableValue: 'valtozo szoveg'};
 
-  constructor(userService: UserService) {
+  constructor(userService: UserService,
+              translateService: TranslateService) {
     this.isLoggedIn$ = userService.isLoggedIn$;
+    translateService.get('WITHVARIABLE', this.translateVariable)
+      .subscribe(
+        res => console.log('translate with variable: ', res)
+      );
+
+    translateService.get(['SIMPLE', 'WITHVARIABLE'], this.translateVariable)
+      .subscribe(
+        res => console.log('translate with variable: ', res)
+      );
   }
 
 }
